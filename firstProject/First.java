bcd07468aefb3597baff9538c8827eedcb9907c1
aaidenplays <aaidenplays@yahoo.com>
2020-09-29 11:42:24 -0500

    wrote first java project!

--- firstProject/First.java
+++ firstProject/First.java
@@ -1,8 +1,14 @@
 import java.lang.*;
+import java.util.*;
 
 class First {
     public static void main(String args[])
     {
-        System.out.println("HelloWorld");
+        String name;
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.println("Please enter your name: ");
+        name = sc.nextLine();
+        System.out.println("Welcome " + name);
     }
 }
